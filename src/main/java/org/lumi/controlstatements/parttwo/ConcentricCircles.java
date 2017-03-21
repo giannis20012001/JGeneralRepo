2ed183de8b3522024ef3d2ab7c0e0abdbdf30b5d
John Tsantilis <i.tsantilis@yahoo.com>
2017-03-21 18:05:06 +0200

    code refactoring addition of ConcentricCircles in controlstatements.parttwo package

---
+++ src/main/java/org/lumi/controlstatements/parttwo/ConcentricCircles.java
@@ -0,0 +1,24 @@
+package org.lumi.controlstatements.parttwo;
+
+import javax.swing.JPanel;
+import java.awt.Graphics;
+
+/**
+ * Created by John Tsantilis
+ * (i [dot] tsantilis [at] yahoo [dot] com A.K.A lumi) on 16/3/2017.
+ */
+
+public class ConcentricCircles extends JPanel {
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for (int i = 0; i < 12; i++) {
+            g.drawOval(140 - (i * 5), 140 - (i * 5),
+                    10 + (i * 10), 10 + (i * 10));
+
+        }
+
+    }
+
+}
